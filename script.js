2db72f103be02a4cf9cce59986852cd9be38eb31
Jaydee1430 <131425913+Jaydee1430@users.noreply.github.com>
2024-02-12 23:02:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('learn-more-link').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('ab').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('sk').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.getElementById('skills').scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('sk1').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.getElementById('skills').scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('pr').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('con').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+    });
+  });
